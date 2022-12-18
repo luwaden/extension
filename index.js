abba3b8174c5a143774721b7520d9cb20bca931b
Dennis <program>
2022-12-18 23:45:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+  let myLeads = ["bimbo ", "tolu ", "shola"]
+  const inputEl = document.getElementById("input-el")
+  const inputBtn = document.getElementById("input-btn")
+  const ulEl = document.getElementById("ul-el")
+
+console.log(ulEl)
+
+  inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log (myLeads)
+  } )
+  for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" 
+  }
+
+ 
+    
+  
+
+        
