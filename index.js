55df38025db23cc288a5383752e356f7b4f7740f
Dennis <program>
2022-12-26 18:14:06 +0100

    first  commit

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 
-  let myLeads = ["bimbo ", "tolu ", "shola"]
+ //   
+ let myLeads = []
   const inputEl = document.getElementById("input-el")
   const inputBtn = document.getElementById("input-btn")
   const ulEl = document.getElementById("ul-el")
 
-console.log(ulEl)
-
+  localStorage.setItem("myLeads", "www.protostreat.com")
+   
   inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log (myLeads)
+    inputEl.value = ""
+    renderLeads ()
+    
   } )
+   
+  function renderLeads() {
+  let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>" 
+    listItems += `
+    <li> 
+       <a target='_blank' href='${myLeads[i]}'>
+       ${myLeads[i]}
+        </a> 
+    </li>
+       `
+  }
+    ulEl.innerHTML = listItems
+ 
   }
 
  
     
   
-
+ 
         
